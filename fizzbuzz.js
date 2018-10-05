5c7750e38504c0aa7d0669733131bb725fab4343
Marcin Rał <marcin@ral.info.pl>
2018-10-05 18:36:37 +0200

    step one

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,2 +1,17 @@
 
 console.log('Wellcome to FizzBuzz!');
+
+
+for(let i=1;i<=100;i++){
+    
+    modulo3 = i%3;
+    modulo5 = i%5;
+    if(modulo3===0){
+        console.log('Fizz');
+    } else if(modulo5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}
+
