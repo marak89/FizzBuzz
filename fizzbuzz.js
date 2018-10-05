ec01a33d22cb24a5c7bb13a1b55f2d62fcb61005
Marcin Rał <marcin@ral.info.pl>
2018-10-05 18:41:41 +0200

    step two

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,7 +6,9 @@ for(let i=1;i<=100;i++){
     
     modulo3 = i%3;
     modulo5 = i%5;
-    if(modulo3===0){
+    if(modulo3===0 && modulo5===0){
+        console.log('FizzBuzz');
+    } else if(modulo3===0){
         console.log('Fizz');
     } else if(modulo5 === 0) {
         console.log('Buzz');
